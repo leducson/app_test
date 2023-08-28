a6ed61fe75cf70bb72bdaaeb1a256d05b71884f9
Le Duc Son <ducson.aptech@gmail.com>
2023-08-28 14:00:59 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,8 @@ set :rbenv_ruby, '2.5.9'
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 
 set :puma_rackup, -> {File.join(current_path, "config.ru")}
-set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
-set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+# set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+# set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
 set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
 set :puma_conf, -> {"#{shared_path}/puma.rb"}
 set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
